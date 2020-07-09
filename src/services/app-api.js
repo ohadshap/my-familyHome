7f8aca7b902ce1fac31e2b9d8c03c414b67b35b7
NadavStutzen <nadstutzen@gmail.com>
2020-07-09 16:08:42 +0300

    changedomain

--- src/services/app-api.js
+++ src/services/app-api.js
@@ -2,15 +2,15 @@ const SERVER_ADDRESS =
   process.env.VUE_APP_SERVER_ADDRESS || 'https://my-family-home.firebaseio.com';
 
 const api = {
-  // homes: () => '/homes.json',
-  // home: args => `/homes/${args.homeId}.json`,
-  // homePics: () => '/homePics.json',
-  // homePic: args => `/homePics/${args.homePicId}.json`
-  homes: () => 'stgHomes.json',
-  home: args => `stgHomes/${args.homeId}.json`,
+  homes: () => '/homes.json',
+  home: args => `/homes/${args.homeId}.json`,
+  homePics: () => '/homePics.json',
+  homePic: args => `/homePics/${args.homePicId}.json`
+  // homes: () => 'stgHomes.json',
+  // home: args => `stgHomes/${args.homeId}.json`,
   // homePics: () => 'stgHomePics.json?orderBy="$key"&limitToLast=150',
-  homePics: () => 'stgHomePics.json?',
-  homePic: args => `stgHomePics/${args.homePicId}.json`,
+  // homePics: () => 'stgHomePics.json?',
+  // homePic: args => `stgHomePics/${args.homePicId}.json`,
   // termsOfService : () => `termsOfService.json`,
   // getTermsOfService : () => `termsOfService/12345667.json`
 };
